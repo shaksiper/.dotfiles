8586ae47d1feba78bc5859af8f9f8d784126528c
Can Berk Cetin <cberkcetin@gmail.com>
2025-02-21 04:31:11 +0300

    refactor(Oil): better config
    Signed-off-by: Can Berk Cetin <cberkcetin@gmail.com>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,1 +1,13 @@
-require("oil").setup()
+require("oil").setup(
+    {
+        columns = {
+            "icon",
+            "size",
+            "mtime",
+        },
+        delete_to_trash = true,
+        keymaps = {
+            ["<BS>"] = { "actions.parent", mode = "n" },
+        },
+    }
+)
