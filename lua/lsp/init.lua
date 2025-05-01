128a1a7c59a2b0464df0b1f6daa7b2469df38ff6
Can Berk Cetin <cberkcetin@gmail.com>
2025-05-01 23:33:26 +0300

    feat(LSP): migrate from Omnisharp to Roslyn
    Retire Omnisharp, since it does not support newer version of .NET
    (.NET > 6)
    
    Signed-off-by: Can Berk Cetin <cberkcetin@gmail.com>

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -420,18 +420,64 @@ nvim_lsp.omnisharp.setup({
 	-- enable_roslyn_analyzers = true,
 	organize_imports_on_format = false,
 	enable_import_completion = true,
+require("roslyn").setup({
+    -- dotnet_cmd = "dotnet",           -- this is the dfault
+    -- roslyn_version = "4.8.0-3.23475.7", -- this is the default
+    -- exe = { "roslyn-language-server" },
+    -- broad_search = true,
+    -- filetypes = { "cs" },
+    config = {
+        -- on_attach = function(client, bufnr)
+        --     monkey_patch_semantic_tokens(client)
+        --     on_attach(client, bufnr)
+        -- end,                         -- required
+        -- on_attach = monkey_patch_semantic_tokens,
+        cmd = {
+            "dotnet",
+            "/home/can/LSP/roslyn/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer.dll",
+            "--logLevel=Debug", "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+            "--stdio"
+        },
+        on_attach = on_attach,
+        capabilities = capabilities, -- required
+        settings = {
+            ["csharp|inlay_hints"] = {
+                csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                csharp_enable_inlay_hints_for_types = true,
+                dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                dotnet_enable_inlay_hints_for_literal_parameters = true,
+                dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                dotnet_enable_inlay_hints_for_other_parameters = true,
+                dotnet_enable_inlay_hints_for_parameters = true,
+                -- dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                -- dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                -- dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+            },
+            ["csharp|code_lens"] = {
+                dotnet_enable_references_code_lens = true,
+                -- dotnet_enable_tests_code_lens = true,
+            },
+            ["csharp|completion"] = {
+                dotnet_show_completion_items_from_unimported_namespaces = true,
+                dotnet_show_name_completion_suggestions = true,
+            },
+            ["csharp|symbol_search"] = {
+                dotnet_search_reference_assemblies = true,
+            },
+            ["csharp|background_analysis"] = {
+                dotnet_analyzer_diagnostics_scope = 'fullSolution',
+                dotnet_compiler_diagnostics_scope = 'fullSolution',
+            },
+        },
+    },
 })
 
 -- nvim_lsp.razor.setup({
 -- 	cmd = { "rzls" },
 -- })
 
--- local pid = vim.fn.getpid() -- not a good LS
--- nvim_lsp.omnisharp.setup({
--- 	cmd = { "OmniSharp", "--languageserver", "--hostPID", tostring(pid) },
--- 	on_attach = on_attach,
--- 	capabilities = capabilities,
--- })
 -- local util = require 'lspconfig.util'
 nvim_lsp.graphql.setup({
 	-- root_dir =  util.root_pattern('.graphqlrc*', '.graphql.config.*', 'graphql.config.*', '.git'),
