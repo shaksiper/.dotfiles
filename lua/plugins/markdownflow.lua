2ec6f103b229532c210c50f9d12669da5a6185e0
Can Berk Cetin <cberkcetin@gmail.com>
2025-05-07 15:04:17 +0300

    chore: format some files

--- lua/plugins/markdownflow.lua
+++ lua/plugins/markdownflow.lua
@@ -1,20 +1,20 @@
 require("mkdnflow").setup({
-	mappings = {
-		MkdnEnter = { { "i", "n", "v" }, "<CR>" },
-		MkdnUpdateNumbering = { { "n" }, "<leader>gn" },
-		MkdnNextLink = { "n", "<C-Tab>" }, -- TAB = C-i in terminals and this breaks the jumplist flow otherwise
-	},
-	modules = {
-		cmp = true,
-	},
-	links = {
-		conceal = true,
-	},
-	tables = {
-		auto_extend_rows = true,
-	},
-	to_do = {
-		symbols = { " ", ">", "x" },
-	},
+    mappings = {
+        MkdnEnter = { { "i", "n", "v" }, "<CR>" },
+        MkdnUpdateNumbering = { { "n" }, "<leader>gn" },
+        MkdnNextLink = { "n", "<C-Tab>" }, -- TAB = C-i in terminals and this breaks the jumplist flow otherwise
+    },
+    modules = {
+        cmp = true,
+    },
+    links = {
+        conceal = true,
+    },
+    tables = {
+        auto_extend_rows = true,
+    },
+    to_do = {
+        symbols = { " ", ">", "x" },
+    },
 })
 require("peek").setup({})
