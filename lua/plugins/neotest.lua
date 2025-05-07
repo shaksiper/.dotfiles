e0762823c45360cc9f3f251cca2f1946bfb32996
Can Berk Cetin <cberkcetin@gmail.com>
2025-05-07 15:11:24 +0300

    feat(plugin): config neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,5 +1,27 @@
 require("neotest").setup({
-	adapters = {
-		require("neotest-dotnet"),
-	},
+    adapters = {
+        require("neotest-dotnet") {
+            discovery_root = "solution",
+            dap = {
+                adapter_name = "coreclr"
+            },
+        },
+    },
+    consumers = {
+        overseer = require("neotest.consumers.overseer"),
+    },
+    overseer = {
+        enabled = true,
+        -- When this is true (the default), it will replace all neotest.run.* commands
+        -- force_default = false,
+    },
+})
+vim.api.nvim_create_autocmd("User", {
+    pattern = "NeotestSummaryOpen",
+    callback = function()
+        vim.defer_fn(
+            function() vim.api.nvim_set_current_win(vim.fn.bufwinid("Neotest Summary")) end,
+            1
+        )
+    end,
 })
