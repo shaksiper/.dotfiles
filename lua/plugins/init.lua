2163c8768ea6ddae2be406be24649e0fdc594fc4
Can Berk Cetin <cberkcetin@gmail.com>
2025-08-25 22:34:59 +0300

    refactor(obsidian.nvim): break config into a new file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -97,34 +97,7 @@ require("neogen").setup({
         },
     },
 })
-require("obsidian").setup({
-    dir = "/mnt/c/Users/CanBerkCetin/Documents/Obsidian/Vispera/",
-    completion = {
-        nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
-    },
-    templates = {
-        subdir = "templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-    },
-    daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "Personal/Daily",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-%m-%d",
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = "%B %-d, %Y",
-    },
-    mappings = {
-        ["gf"] = vim.keymap.set("n", "gf", function()
-            if require("obsidian").util.cursor_on_markdown_link() then
-                return "<cmd>ObsidianFollowLink<CR>"
-            else
-                return "gf"
-            end
-        end, { noremap = false, expr = true }),
-    },
-})
+require("plugins.obsidian")
 -- require("headlines").setup({
 -- 	markdown = {
 -- 		fat_headlines = false,
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,52 @@
+require("obsidian").setup({
+    dir = "/mnt/c/Users/CanBerkCetin/Documents/Obsidian/Vispera/",
+    workspaces = {
+        {
+            name = "work",
+            path = "/mnt/c/Users/CanBerkCetin/Documents/Obsidian/Vispera/"
+        }
+    },
+    completion = {
+        blink = true,
+        min_chars = 2,
+        -- nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
+    },
+    templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+    },
+    daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "Personal/Daily",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+    },
+    -- delete after deprecation
+    statusline = {
+        enabled = false,
+    },
+    footer = {
+        enabled = true,
+    },
+    callbacks = {
+        enter_note = function(_, note)
+            -- we can set keymaps here
+            vim.keymap.set("n", "gf", function()
+                if require("obsidian").util.cursor_on_markdown_link() then
+                    return "<cmd>ObsidianFollowLink<CR>"
+                else
+                    return "gf"
+                end
+            end, {
+                buffer = note.bufnr,
+                desc = "Follow Link",
+                noremap = false,
+                expr = true
+            })
+        end,
+    },
+    legacy_commands = false,
+})
