0696ed3a7b97a7b0f8e198ddc5ffbb744c6f7f2c
Can Berk Cetin <cberkcetin@gmail.com>
2025-08-25 22:32:05 +0300

    feat(neotest): use newly developed vstest runner for .NET

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,11 +1,16 @@
 require("neotest").setup({
     adapters = {
-        require("neotest-dotnet") {
-            discovery_root = "solution",
-            dap = {
-                adapter_name = "coreclr"
-            },
-        },
+        -- require("neotest-dotnet") {
+        --     discovery_root = "solution",
+        --     dap = {
+        --         adapter_name = "coreclr"
+        --     },
+        -- },
+        require("neotest-vstest") {
+            dap_settings = {
+                type = "coreclr",
+            }
+        }
     },
     consumers = {
         overseer = require("neotest.consumers.overseer"),
@@ -16,12 +21,12 @@ require("neotest").setup({
         -- force_default = false,
     },
 })
-vim.api.nvim_create_autocmd("User", {
-    pattern = "NeotestSummaryOpen",
-    callback = function()
-        vim.defer_fn(
-            function() vim.api.nvim_set_current_win(vim.fn.bufwinid("Neotest Summary")) end,
-            1
-        )
-    end,
-})
+-- vim.api.nvim_create_autocmd("User", {
+--     pattern = "NeotestSummaryOpen",
+--     callback = function()
+--         vim.defer_fn(
+--             function() vim.api.nvim_set_current_win(vim.fn.bufwinid("Neotest Summary")) end,
+--             1
+--         )
+--     end,
+-- })
