d72b2499d88cf9dcf8ce1ebef6db99a54daa91ea
Can Berk Cetin <cberkcetin@gmail.com>
2026-02-20 00:10:36 +0300

    refactor(Plugin): use new repository home for Leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -20,4 +20,36 @@ require("leap-spooky").setup({
 	-- at the cursor position if the unnamed register is in use.
 	paste_on_remote_yank = true,
 })
-require("flit").setup()
+-- require("flit").setup()
+do
+	-- Returns an argument table for `leap()`, tailored for f/t-motions.
+	local function as_ft(key_specific_args)
+		local common_args = {
+			inputlen = 1,
+			inclusive = true,
+			-- To limit search scope to the current line:
+			-- pattern = function (pat) return '\\%.l'..pat end,
+			opts = {
+				labels = "", -- force autojump
+				safe_labels = vim.fn.mode(1):match("o") and "" or nil, -- [1]
+				case_sensitive = true, -- [2]
+			},
+		}
+		return vim.tbl_deep_extend("keep", common_args, key_specific_args)
+	end
+
+	local clever = require("leap.user").with_traversal_keys -- [3]
+	local clever_f = clever("f", "F")
+	local clever_t = clever("t", "T")
+
+	for key, args in pairs({
+		f = { opts = clever_f },
+		F = { backward = true, opts = clever_f },
+		t = { offset = -1, opts = clever_t },
+		T = { backward = true, offset = 1, opts = clever_t },
+	}) do
+		vim.keymap.set({ "n", "x", "o" }, key, function()
+			require("leap").leap(as_ft(args))
+		end)
+	end
+end
