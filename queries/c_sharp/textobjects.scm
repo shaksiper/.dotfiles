cdd1bfaeb5f24e9af8b69b4ffef400cbbf09e8e8
Can Berk Cetin <cberkcetin@gmail.com>
2026-02-20 01:14:50 +0300

    feat(C#): custom treesitter queries & move treesitter.start logic

--- queries/c_sharp/textobjects.scm
+++ queries/c_sharp/textobjects.scm
@@ -1,1 +1,59 @@
+;; extends
 (method_declaration returns:(_) @returns)
+
+; (argument (_) @parameter.inner) @parameter.outer
+
+;; Match the empty argument list (fallback case)
+(argument_list) @parameter.empty
+
+; New parameter Text Objects
+
+; parameters
+(parameter_list
+  "," @parameter.outer
+  .
+  (_) @parameter.inner @parameter.outer)
+
+(parameter_list
+  .
+  (_) @parameter.inner @parameter.outer
+  .
+  ","? @parameter.outer)
+
+(argument_list
+  "," @parameter.outer
+  .
+  (_) @parameter.inner @parameter.outer)
+
+(argument_list
+  .
+  (_) @parameter.inner @parameter.outer
+  .
+  ","? @parameter.outer)
+
+; ( (argument (_) @parameter.inner) ","? ) @parameter.outer
+
+; ( ( (argument (_) @argument ) @argument ( "," )+) @argument.outer)
+
+(expression_statement) @statement.outer
+
+(local_declaration_statement) @statement.outer
+
+(comment)+ @comments
+
+(method_declaration) @function.outer
+
+(method_declaration
+ body: (_)
+          @function.inner
+          ; (#strip! @function.inner "^{" "}$")
+          )
+
+
+; (method_declaration
+;   body: (_
+;     (_)* @function.prev
+;     .
+;     (_)* @function.next
+;     )
+;   (#make-range! "function.inner" @function.prev @function.next))
