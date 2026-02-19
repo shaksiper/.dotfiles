8a444685096e8f1fbe29cf3841d2ab2fdd94678e
Can Berk Cetin <cberkcetin@gmail.com>
2026-02-20 00:53:09 +0300

    feat(Snacks): enable terminal & take it for a ride for a while

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,8 +9,47 @@ vim.api.nvim_create_autocmd("User", {
 		end
 	end,
 })
+
 Snacks.setup({
 	bigfile = { enabled = true },
+	terminal = {
+		enabled = true,
+		bo = {
+			filetype = "snacks_terminal",
+		},
+		wo = {},
+		stack = true, -- when enabled, multiple split windows with the same position will be stacked together (useful for terminals)
+		keys = {
+			q = "hide",
+			gf = function(self)
+				local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+				if f == "" then
+					Snacks.notify.warn("No file under cursor")
+				else
+					self:hide()
+					vim.schedule(function()
+						vim.cmd("e " .. f)
+					end)
+				end
+			end,
+			term_normal = {
+				"<esc>",
+				function(self)
+					self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+					if self.esc_timer:is_active() then
+						self.esc_timer:stop()
+						vim.cmd("stopinsert")
+					else
+						self.esc_timer:start(200, 0, function() end)
+						return "<esc>"
+					end
+				end,
+				mode = "t",
+				expr = true,
+				desc = "Double escape to normal mode",
+			},
+		},
+	},
 	-- dashboard = { enabled = true },
 	-- explorer = { enabled = true },
 	-- indent = { enabled = true },
