10f52dc34a37c69faae928c7ac21a159ee5d3682
Can Berk Cetin <cberkcetin@gmail.com>
2026-02-20 00:41:59 +0300

    feat(Neotest): add notifications for test actions

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,3 +1,4 @@
+local Snakcs = require("snacks")
 require("neotest").setup({
 	adapters = {
 		-- require("neotest-dotnet")({
@@ -14,6 +15,57 @@ require("neotest").setup({
 	},
 	consumers = {
 		overseer = require("neotest.consumers.overseer"),
+		notification = function(client)
+			local notification_id = "test_start_notification"
+			client.listeners.run = function(_) -- (adapter_id, results)
+				vim.notify("Tests running...", vim.log.levels.INFO, {
+					title = "Neotest",
+					id = notification_id,
+					timeout = false, -- Keep it open until we manually close it
+					layout = { top = 5 },
+					opts = function(notif)
+						notif.icon = Snakcs.util.spinner()
+					end,
+				})
+			end
+			client.listeners.starting = function(_)
+				vim.notify("Tests being discovered", vim.log.levels.INFO, {
+					title = "Neotest",
+					id = notification_id,
+					timeout = false, -- Keep it open until we manually close it
+					layout = { top = 5 },
+					opts = function(notif)
+						notif.icon = Snakcs.util.spinner()
+					end,
+				})
+			end
+			client.listeners.started = function(_)
+				vim.notify("Tests discovered", vim.log.levels.INFO, {
+					title = "Neotest",
+					id = notification_id,
+					timeout = 5000,
+				})
+			end
+			client.listeners.results = function(_, results)
+				local has_failed = false
+				for _, result in pairs(results) do
+					if result.status == "failed" then
+						has_failed = true
+						break
+					end
+				end
+
+				local level = has_failed and vim.log.levels.ERROR or vim.log.levels.INFO
+				local msg = has_failed and "Tests Failed" or "Tests Passed"
+				local icon = ""
+
+				vim.notify(msg, level, {
+					title = "Neotest",
+					id = notification_id,
+					timeout = 3000,
+				})
+			end
+		end,
 	},
 	overseer = {
 		enabled = true,
@@ -21,6 +73,7 @@ require("neotest").setup({
 		-- force_default = false,
 	},
 })
+
 -- vim.api.nvim_create_autocmd("User", {
 --     pattern = "NeotestSummaryOpen",
 --     callback = function()
