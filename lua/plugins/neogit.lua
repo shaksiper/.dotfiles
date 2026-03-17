7fae74eb3015e3b30a4217b9b763852807c4654a
Can Berk Cetin <cberkcetin@gmail.com>
2026-03-18 02:10:48 +0300

    feat(Neogit): use ASCII graph on neovide

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,3 +1,12 @@
+local graph_style = "kitty"
+if vim.g.neovide then
+	graph_style = "unicode"
+end
 require("neogit").setup({
-	graph_style = "kitty",
+	graph_style = graph_style,
+	diff_viewer = "codediff",
+    -- highlight = { -- somehow it is not fallback to default
+    --     red = "red",
+    --     purple = "purple"
+    -- }
 })
