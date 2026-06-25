1a6c7cd8eb72c833dc45130b07a295ab374880f0
C. Berk Cetin <cberkcetin@gmail.com>
2026-06-25 04:16:56 +0300

    refactor(usercommand): use concise diagnostic toggle

--- lua/plugins/custom-usercommands.lua
+++ lua/plugins/custom-usercommands.lua
@@ -1,12 +1,13 @@
 vim.api.nvim_create_user_command("ToggleDiagnostic", function()
 	-- get current buffer
-	local buf = vim.api.nvim_get_current_buf()
-	-- check if the diagnostics hidden for the current buffer
-	if vim.diagnostic.is_disabled(buf) then
-		-- show the diagnostics
-		vim.diagnostic.enable(buf)
-	else
-		-- hide the diagnostics
-		vim.diagnostic.disable(buf)
-	end
+	-- local buf = vim.api.nvim_get_current_buf()
+	-- -- check if the diagnostics hidden for the current buffer
+	-- if vim.diagnostic.is_disabled(buf) then
+	-- 	-- show the diagnostics
+	-- 	vim.diagnostic.enable(buf)
+	-- else
+	-- 	-- hide the diagnostics
+	-- 	vim.diagnostic.disable(buf)
+	-- end
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled({ bufnr = 0 }), { bufnr = 0 })
 end, {})
